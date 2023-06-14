dd362c6fcdee0868c461b2d6200106db30081b90
julien rocroi <julien.rocroi@gmail.com>
2023-06-14 17:10:25 +0200

    yelp

---
+++ app/models/restaurant.rb
@@ -0,0 +1,5 @@
+class Restaurant < ApplicationRecord
+  has_many :reviews, dependent: :destroy
+  CATEGORY = ["chinese", "italian", "japanese", "french", "belgian"]
+  validates :category, inclusion: {in: CATEGORY}
+end
